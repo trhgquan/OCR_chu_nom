84b4f23d1d67e42c4c7ce37db908bb609ad2ea2c
trhgquan <quanthcstpbt@gmail.com>
2021-12-16 12:14:12 +0700

    Split train and test

--- create_data.py
+++ create_data.py
@@ -1,7 +1,9 @@
 import os
-import pandas as pd
+import random
 
 DATA_DIR = 'data_real'
+train_csv = 'train_1.csv'
+test_csv = 'test_1.csv'
 
 # train.csv would consists of files inside MTH1000 and TKH
 # test.csv would consists of files inside MTH1200
@@ -21,25 +23,63 @@ for file in os.listdir(os.path.join(DATA_DIR, 'img')):
 
 unicode_translate = dict()
 
-with open('train.csv', 'w+', encoding = 'utf-8') as fw:
+# Get random images to put to train.csv
+train_picked = []
+test_picked = []
+
+limit_test = 1000
+
+for i in range(0, len(labels)):
+    if len(test_picked) < limit_test:
+        chosen_one = random.randint(0, 1)
+
+        if chosen_one == 1:
+            test_picked.append(i)
+
+with open(train_csv, 'w+', encoding = 'utf-8') as fw:
     print('image_id,labels', file = fw)
 
-    for filename, label_in_file in labels.items():
-        print(filename, file = fw, end = ',')
-        print('Doing', filename)
-        for label in label_in_file:
-            splitted = label.split(' ')
 
-            if hex(ord(splitted[0])) not in unicode_translate:
-                unicode_translate[hex(ord(splitted[0]))] = splitted[0]
+with open(test_csv, 'w+', encoding = 'utf-8') as fw:
+    print('image_id,labels', file = fw)
 
-            splitted[0] = hex(ord(splitted[0]))
-            splitted[1] = int(float(splitted[1]))
-            splitted[2] = int(float(splitted[2]))
-            splitted[3] = int(float(splitted[3]))
-            for s in splitted:
-                print(s, file = fw, end = ' ')
-        print(file = fw)
+with open(train_csv, 'w+', encoding = 'utf-8') as train_f:
+    print('image_id,labels', file = train_f)
+    with open(test_csv, 'w+', encoding = 'utf-8') as test_f:
+        print('image_id,labels', file = test_f)
+        for index, (filename, label_in_file) in enumerate(labels.items()):
+            if index in test_picked:
+                print(filename, file = test_f, end = ',')
+                print('Printing', filename, 'to test.csv')
+                for label in label_in_file:
+                    splitted = label.split(' ')
+
+                    if hex(ord(splitted[0])) not in unicode_translate:
+                        unicode_translate[hex(ord(splitted[0]))] = splitted[0]
+
+                    splitted[0] = hex(ord(splitted[0]))
+                    splitted[1] = int(float(splitted[1]))
+                    splitted[2] = int(float(splitted[2]))
+                    splitted[3] = int(float(splitted[3]))
+                    for s in splitted:
+                        print(s, file = test_f, end = ' ')
+                print(file = test_f)
+            else:
+                print(filename, file = train_f, end = ',')
+                print('Printing', filename, 'to train.csv')
+                for label in label_in_file:
+                    splitted = label.split(' ')
+
+                    if hex(ord(splitted[0])) not in unicode_translate:
+                        unicode_translate[hex(ord(splitted[0]))] = splitted[0]
+
+                    splitted[0] = hex(ord(splitted[0]))
+                    splitted[1] = int(float(splitted[1]))
+                    splitted[2] = int(float(splitted[2]))
+                    splitted[3] = int(float(splitted[3]))
+                    for s in splitted:
+                        print(s, file = train_f, end = ' ')
+                print(file = train_f)
 
 with open('unicode_translate.csv', 'w+', encoding = 'utf-8') as fw:
     print('unicode,value', file = fw)
